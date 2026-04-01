28c40ab0f373738f4a3b81cf984c6606cc9ae66e
Ajay-karuppaiyan <ajaymailbox97@gmail.com>
2026-04-01 15:13:00 +0530

    google client id changed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 // Env variable for Google Client ID
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
+const GOOGLE_CLIENT_ID = "9842957963-mrch5rrcdvvln351nratopgbb8ohvs5r.apps.googleusercontent.com"
 console.log("GOOGLE_CLIENT_ID")
 createRoot(document.getElementById('root')).render(
   <StrictMode>
